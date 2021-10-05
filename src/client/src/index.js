0cf06b5df36a633d4003da44bb2381fc816e5c5f
ar1fshaikh <76391543+0x7EVEN@users.noreply.github.com>
2021-10-05 15:09:36 +0530

    Add files via upload

---
+++ src/client/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
+import {LoginProvider} from './context/loginContext';
+import LoginContextProvider from './components/Contexts/LoginContext';
+import RegisterContextProvider from './components/Contexts/RegisterContext';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <LoginContextProvider>
+        <RegisterContextProvider>
+          <LoginProvider>
+            <App />
+          </LoginProvider>
+        </RegisterContextProvider>
+      </LoginContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
